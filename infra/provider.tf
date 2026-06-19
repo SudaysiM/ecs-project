0b53b712bc646a55de3df760b0e864ca49799355
SudaysiM <sudaysim@gmail.com>
2026-06-19 16:08:01 +0300

    add state locking to s3 backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,9 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "sudaysi-terraform-state"
-    key    = "ecs-project/terraform.tfstate"
-    region = "eu-west-2"
+    bucket       = "sudaysi-terraform-state"
+    key          = "ecs-project/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
   }
 }
 
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,7 @@
 variable "route53_zone_id" {
   default = "Z0586179MKGJ75OHZTUF"
 }
+
+variable "aws_account_id" {
+  default = "107001694252"
+}
